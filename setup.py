56ad87d21f896e1eee71e4673a7f45ef761ea5d1
Angel <ahsec>
2018-05-20 14:52:49 -0400

    Structure project with setup.py and MAKEFILE

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='UTF-8', mode='r') as fopen:
+    readme = fopen.read()
+
+setup(
+        name='pgbackup',
+        version='0.1.0',
+        description='Database backups locally or to AWS S3',
+        long_description=readme,
+        author='Angel',
+        author_email='angel@gmail.com',
+        packages=find_packages('src'),
+        package_dir={'':'src'},
+        install_requires=[]
+    )
+
