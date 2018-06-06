47320adf8d4b254cff3f9fe7bce787795808a30b
Angel <ahsec>
2018-06-05 23:23:53 -0400

    Local storage tests and functions. Adding local storage tests, and functions code.

--- src/pgbackup/pgdump.py
+++ src/pgbackup/pgdump.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 def dump(url):
     try:
---
+++ tests/test_storage.py
@@ -0,0 +1,15 @@
+import tempfile
+from pgbackup import storage
+
+def test_storing_file_locally():
+    """
+    Writes content from one file-like object to another.
+    """
+    infile = tempfile.TemporaryFile('r+b')
+    infile.write(b'Testing')
+    infile.seek(0)
+
+    outfile = tempfile.NamedTemporaryFile(delete=False)
+    storage.local(infile, outfile)
+    with open(outfile.name, 'rb') as f:
+        assert f.read() == b'Testing'
