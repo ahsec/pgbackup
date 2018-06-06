defd4904e84c1c4059425f9ef1c92491d8c8b5e1
Angel <ahsec>
2018-06-05 22:46:08 -0400

    Implementing postgresql tool validation. validating that psql binary is installed in the system.

---
+++ src/pgbackup/pgdump.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def dump(url):
+    try:
+        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE)
+    except OSError as err:
+        print(f'Error: {err}')
+        sys.exit(1)
--- tests/test_pgdump.py
+++ tests/test_pgdump.py
@@ -11,3 +11,11 @@ def test_dump_calls_pg_dump(mocker):
     mocker.patch('subprocess.Popen')
     assert pgdump.dump(url)
     subprocess.Popen.assert_called_with(['pg_dump', url], stdout=subprocess.PIPE)
+
+def test_dump_handles_oserror(mocker):
+    """
+    ppgdump.dump returns a reasonable error if pg_dump isn't installed.
+    """
+    mocker.patch('subprocess.Popen', side_effect=OSError("no such file"))
+    with pytest.raises(SystemExit):
+        pgdump.dump(url)
