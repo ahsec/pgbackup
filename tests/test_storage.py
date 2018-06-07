1d315593dda25f3175138be4a650f59110e23ef0
Angel <ahsec>
2018-06-06 23:18:26 -0400

    Implement S3 interactions.

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,5 +1,13 @@
 import tempfile
+import pytest
 from pgbackup import storage
+
+@pytest.fixture
+def infile():
+    infile = tempfile.TemporaryFile('r+b')
+    infile.write(b'Testing')
+    infile.seek(0)
+    return infile
 
 def test_storing_file_locally():
     """
@@ -13,3 +21,17 @@ def test_storing_file_locally():
     storage.local(infile, outfile)
     with open(outfile.name, 'rb') as f:
         assert f.read() == b'Testing'
+
+def test_storing_file_on_s3(mocker, infile):
+    """
+    Writes content from one readable to S3.
+    """
+    client = mocker.Mock()
+    storage.s3(client,
+            infile,
+            "python-academy",
+            "file-name")
+    client.upload_fileobj.assert_called_with(
+            infile,
+            "python-academy",
+            "file-name")
